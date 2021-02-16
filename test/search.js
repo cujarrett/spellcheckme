db7bb971d6a516cc173bfc38debe5fd1162e8770
Matt Jarrett <cujarrett@gmail.com>
2021-02-16 17:49:41 -0600

    fix: removes did you mean tests

--- test/search.js
+++ test/search.js
@@ -3,7 +3,7 @@ const test = require("tape-async")
 const { search } = require("../src/search.js")
 
 test("Integration - search", async (assert) => {
-  assert.plan(10)
+  assert.plan(8)
   let response = await search("JavaScript")
   assert.equal(response.exitCode, 0, "Valid spelling exit code verified")
   assert.equal(response.message, "JavaScript 👍", "Valid spelling message verified")
@@ -12,10 +12,6 @@ test("Integration - search", async (assert) => {
   assert.equal(response.exitCode, 1, "\"Showing results for\" invalid spelling search exit code verified")
   // eslint-disable-next-line max-len
   assert.equal(response.message, "JavaScriptz👎 JavaScript 👍", "\"Showing results for\" invalid spelling search message verified")
-  response = await search("tomoto")
-  assert.equal(response.exitCode, 1, "\"Did you Mean\" invalid spelling search exit code verified")
-  // eslint-disable-next-line max-len
-  assert.equal(response.message, "tomoto👎 tomato 👍", "\"Did you Mean\" invalid spelling search message verified")
   response = await search("sfhshjfshdfjhsdiufh")
   assert.equal(response.exitCode, 1, "No results found exit code verified")
   // eslint-disable-next-line max-len
